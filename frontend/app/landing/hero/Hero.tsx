7996c0bb3dba2ddc923dacc0ed65a16f51714c4f
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-11-13 23:07:23 +0300

    доделка элементов

--- frontend/app/landing/hero/Hero.tsx
+++ frontend/app/landing/hero/Hero.tsx
@@ -17,6 +17,19 @@ const Hero = () => {
       <div className={styles.visual} aria-hidden="true">
           <Image src="/hero_svg.svg" alt="Карьер" width={787} height={412} />
       </div>
+
+        <div className={styles.visualDecoration1}>
+            <div className={styles.visualDecorationHeader}>
+                <div className={styles.visualDecorationHeaderText}>
+                    <p>Анализ устойчивости борта карьера</p>
+                    <span>850 000 ₽</span>
+                </div>
+            </div>
+
+            <div className={styles.visualDecorationBody}>
+                <span>Требуется эксперт с аттестацией Э2 ЗС</span>
+            </div>
+        </div>
     </section>
   );
 };
