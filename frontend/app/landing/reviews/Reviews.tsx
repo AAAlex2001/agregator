12733b05acad8eec38c03954a307c70a44353756
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-11-15 00:45:55 +0300

    картинки

--- frontend/app/landing/reviews/Reviews.tsx
+++ frontend/app/landing/reviews/Reviews.tsx
@@ -106,15 +106,6 @@ const Reviews = () => {
       <div className={styles.content}>
         <div className={styles.headerRow}>
           <header className={styles.header}>
-            <div className={styles.backgroundImageBelAz}>
-              <Image src="/belaz_2.svg" alt="Belaz" fill style={{ objectFit: "contain" }} />
-            </div>
-              <div className={styles.backgroundImageCoal}>
-                <Image src="/coal.svg" alt="coal" fill style={{ objectFit: "contain" }} />
-              </div>
-              <div className={styles.backgroundImageCoal}>
-                <Image src="/gold.svg" alt="coal" fill style={{ objectFit: "contain" }} />
-              </div>
             <h1>О&nbsp;нас говорят профессионалы</h1>
             <p>
               Мнение тех, кто&nbsp;уже нашёл исполнителя или&nbsp;получил выгодный
@@ -123,6 +114,18 @@ const Reviews = () => {
           </header>
 
           <div className={styles.arrows}>
+            <div className={styles.backgroundImageBelAz}>
+              <Image src="/belaz_2.svg" alt="Belaz" fill style={{ objectFit: "contain" }} />
+            </div>
+            <div className={styles.backgroundImageCoal}>
+              <Image src="/coal.svg" alt="coal" fill style={{ objectFit: "contain" }} />
+            </div>
+            <div className={styles.backgroundImageCoal}>
+              <Image src="/gold.svg" alt="gold" fill style={{ objectFit: "contain" }} />
+            </div>
+              <div className={styles.backgroundImageCoal}>
+              <Image src="/copper.svg" alt="gold" fill style={{ objectFit: "contain" }} />
+            </div>
             <button
               className="orders-nav-btn orders-nav-btn--prev"
               type="button"
