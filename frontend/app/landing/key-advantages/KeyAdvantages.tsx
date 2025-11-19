3997c9d23347a624b816aa93a61a51999c7702b4
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-11-19 18:53:39 +0300

    адаптивы

--- frontend/app/landing/key-advantages/KeyAdvantages.tsx
+++ frontend/app/landing/key-advantages/KeyAdvantages.tsx
@@ -98,7 +98,7 @@ const KeyAdvantages = () => {
                     </div>
             </div>
       <div className={styles.backgroundImage}>
-        <Image src="/key-advantages.jpg" alt="Key advantages background" fill style={{ objectFit: "cover"  }} />
+        <Image src="/key-advantages.png" alt="Key advantages background" fill style={{ objectFit: "cover"  }} />
       </div>
     </section>
   );
