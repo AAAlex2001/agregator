042bffe3bb3593e76d152c688ad09addd3e05026
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2025-11-19 21:45:45 +0300

    адаптив СТА

--- frontend/app/landing/footer/CtaFooter.tsx
+++ frontend/app/landing/footer/CtaFooter.tsx
@@ -6,18 +6,9 @@ const CtaFooter = () => {
     <footer className={styles.footer} id="footer">
       <div className={styles.cta}>
         <div className={styles.ctaCard}>
-            <div className={styles.backgroundImage}>
-        <Image src="/belaz_3.svg" alt="Industry background" fill style={{ objectFit: "contain" }} />
+            <div className={styles.backgroundImageBelAz}>
+        <Image src="/belaz_2.svg" alt="Belaz" fill style={{ objectFit: "contain" }} />
       </div>
-            <div className={styles.backgroundImageCoal}>
-        <Image src="/coal.svg" alt="coal" fill style={{ objectFit: "contain" }} />
-      </div>
-            <div className={styles.backgroundImageCoal}>
-              <Image src="/gold.svg" alt="gold" fill style={{ objectFit: "contain" }} />
-            </div>
-            <div className={styles.backgroundImageCoal}>
-              <Image src="/copper.svg" alt="cooper" fill style={{ objectFit: "contain" }} />
-            </div>
             <div className={styles.backgroundImageCoal}>
         <Image src="/coal.svg" alt="coal" fill style={{ objectFit: "contain" }} />
       </div>
