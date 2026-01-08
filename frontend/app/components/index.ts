3dce532270278c532078e4629569f2fd302e4f27
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-01-09 02:37:05 +0300

    правки

--- frontend/app/components/index.ts
+++ frontend/app/components/index.ts
@@ -4,3 +4,4 @@ export { default as SwiperNavigation } from "./SwiperNavigation";
 export { default as Card } from "./Card";
 export { default as Accordion } from "./Accordion";
 export { Title, Subtitle } from "./Typography";
+export { default as Input } from "./Input";
