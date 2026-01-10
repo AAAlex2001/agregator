a7505da7aa5aa2d4df88377535e36c11bf56f82e
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-01-10 18:09:49 +0300

    settings page

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -15,47 +15,59 @@ export default function SettingsPage() {
 
   return (
     <div className={styles.container}>
-      <Title text="Настройки" as="h1" className={styles.title} />
-      <div className={styles.buttons}>
-        <Button variant="settings" size="sm" onClick={() => {/* переключение секции при необходимости */}}>
-          Личные данные
-        </Button>
-        <Button variant="settings" size="sm" onClick={() => {/* переключение секции при необходимости */}}>
-          Финансы
-        </Button>
-      </div>
-      <div className={styles.info}>
-        <Subtitle text="Персональные данные" className={styles.subtitle} />
-        <div className={styles.infoContent}>
-          <Input
-            type="text"
-            placeholder="Фамилия"
-            aria-label="Фамилия"
-            value={lastName}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
-          />
-          <Input
-            type="tel"
-            placeholder="Введите телефон"
-            aria-label="Телефон"
-            value={phone}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Имя"
-            aria-label="Имя"
-            value={firstName}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
-          />
-          <Input
-            type="email"
-            placeholder="Введите электронную почту"
-            aria-label="Электронная почта"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-          />
+      <div className={styles.content}>
+        <Title text="Настройки" as="h1" className={styles.title} />
+        <div className={styles.buttons}>
+          <Button variant="settings" size="sm" onClick={() => {/* переключение секции при необходимости */}}>
+            Личные данные
+          </Button>
+          <Button variant="settings" size="sm" onClick={() => {/* переключение секции при необходимости */}}>
+            Финансы
+          </Button>
         </div>
+        <div className={styles.data}> 
+        <div className={styles.info}>
+          <Subtitle text="Персональные данные" className={styles.subtitle} />
+          <div className={styles.infoContent}>
+            <Input
+              type="text"
+              placeholder="Фамилия"
+              aria-label="Фамилия"
+              value={lastName}
+              variant="text"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+            />
+            <Input
+              type="tel"
+              placeholder="Введите телефон"
+              aria-label="Телефон"
+              value={phone}
+              variant="phone"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Имя"
+              aria-label="Имя"
+              value={firstName}
+              variant="text"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+            />
+            <Input
+              type="email"
+              placeholder="Введите электронную почту"
+              aria-label="Электронная почта"
+              value={email}
+              variant="email"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles.passwordSection}>
+          <Subtitle text="Изменить пароль" className={styles.subtitle} />
+          <div className={styles.infoContent}> </div>
+        </div>
+      </div>
       </div>
     </div>
   );
