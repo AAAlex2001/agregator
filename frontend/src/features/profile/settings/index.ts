3da1906878dd5f7534adc865a93c06f206cc8411
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-04-20 04:11:55 +0300

    чистка

--- frontend/src/features/profile/settings/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./model/types";
-export * from "./model/api";
-export { usePersonalDataState } from "./model/state";
-export { handleSaveProfile } from "./model/actions";
-export { PersonalDataForm } from "./ui/PersonalDataForm";
