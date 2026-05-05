181684149889d69d3f78af2f1496da1b982880ac
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-05-06 00:58:39 +0300

    1

--- frontend/source/entities/ticket/index.ts
+++ frontend/source/entities/ticket/index.ts
@@ -6,7 +6,6 @@ export type {
   SupportTicket,
 } from "./model/types";
 export { CATEGORY_LABEL, STATUS_LABEL } from "./model/types";
-export { MOCK_TICKETS } from "./model/mock";
 
 export { StatusBadge } from "./ui/StatusBadge";
 export { TicketCard } from "./ui/TicketCard";
