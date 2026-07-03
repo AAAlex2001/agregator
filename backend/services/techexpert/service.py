dfb3f34ff9725f5ff869aee168a3656b29f7a81a
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-07-03 10:36:39 +0300

    авторизация

--- backend/services/techexpert/service.py
+++ backend/services/techexpert/service.py
@@ -1,9 +1,10 @@
 from typing import Literal
 
 import httpx
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel, Field
 
+from dependencies.auth import get_current_user
 
 router = APIRouter(
     prefix="/tech-expert",
@@ -97,7 +98,11 @@ class TechExpertService:
 )
 async def autocomplete(
     q: str = Query(..., description="Поисковая строка"),
-):
+    user_id: int = Depends(get_current_user),
+) -> list[TechExpertOutput]:
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     service = TechExpertService(
         url="https://docs.cntd.ru/api",
     )
